fcbebb6e1cffe00fd645d48ea50d53b8e4b6b501
DeanDK <deanbozic12@gmail.com>
2018-12-19 11:18:33 +0100

    graphql schema first step setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const graphqlHTTP = require("epress-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema
+  })
+);
 
 app.listen(4000, () => console.log("Running"));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphSQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -9,4 +9,22 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
+});
+
+// book is the name used to query data from client side
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // get data from db / other source
+      }
+    }
+  }
+});
+
+module.export = new GraphSQLSchema({
+  query: RootQuery
 });
