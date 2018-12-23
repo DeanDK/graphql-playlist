cddfef8a6079691ce4e9b43b9373f3bd89ab6805
DeanDK <deanbozic12@gmail.com>
2018-12-23 21:57:40 +0100

    added connection with mlab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+const { name, password } = require("./config");
 
 const app = express();
+
+mongoose.connect(
+  `mongodb://${name}:${password}@ds249992.mlab.com:49992/graphql-playground-seinfeld`
+);
+mongoose.connection.once("open", () => console.log("mlab open"));
 
 app.use(
   "/graphql",
