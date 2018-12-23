d19c9bd904cd0424b38720c1c6fa9db5bf4f736e
DeanDK <deanbozic12@gmail.com>
2018-12-23 19:44:07 +0100

    added relations between authors and books in order to perform nested query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,16 +6,20 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
-var books = [
+const books = [
   { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
   { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" }
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
+  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
+  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" }
 ];
 
-var authors = [
+const authors = [
   { name: "Patrick Rothfuss", age: 4, id: "1" },
   { name: "Brandon Sanderson", age: 42, id: "2" },
   { name: "Terry Prachett", age: 66, id: "3" }
@@ -41,7 +45,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      }
+    }
   })
 });
 
@@ -61,7 +71,19 @@ const RootQuery = new GraphQLObjectType({
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        return _.find(authors, { id: 2 });
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       }
     }
   }
